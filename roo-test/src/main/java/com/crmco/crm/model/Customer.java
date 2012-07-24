3ea1278c66a3eaf669cdfd10f92342b6a23c7009
Damian Pisaturo <damian.pisaturo@teracode.com>
2012-07-24 19:41:28 -0300

    Adding Spring Roo test project.

---
+++ roo-test/src/main/java/com/crmco/crm/model/Customer.java
@@ -0,0 +1,15 @@
+package com.crmco.crm.model;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Customer {
+
+    @NotNull
+    private String firstName;
+}
