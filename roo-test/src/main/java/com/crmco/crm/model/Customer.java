f845f320f5e1bb528db27f050d4afcc7dadf10da
Damian Pisaturo <damian.pisaturo@teracode.com>
2012-07-25 17:13:20 -0300

    Adding REST API for Customer.

--- roo-test/src/main/java/com/crmco/crm/model/Customer.java
+++ roo-test/src/main/java/com/crmco/crm/model/Customer.java
@@ -1,13 +1,16 @@
 package com.crmco.crm.model;
 
 import javax.validation.constraints.NotNull;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
+@RooJson
 public class Customer {
 
     @NotNull
--- roo-test/src/main/java/com/crmco/crm/web/CustomerController.java
+++ roo-test/src/main/java/com/crmco/crm/web/CustomerController.java
@@ -1,6 +1,7 @@
 package com.crmco.crm.web;
 
 import com.crmco.crm.model.Customer;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,5 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customers")
 @Controller
 @RooWebScaffold(path = "customers", formBackingObject = Customer.class)
+@RooWebJson(jsonObject = Customer.class)
 public class CustomerController {
 }
